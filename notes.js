3d9726b5282965ab35a58488b9b15a6a7605452d
James Fisher <jfisher396@hotmail.com>
2021-01-07 21:50:30 -0800

    remove note function created

--- notes.js
+++ notes.js
@@ -6,11 +6,21 @@ const getNotes = function () {
 
 const addNote = function (title, body) {
   const notes = loadNotes();
-  notes.push({
-    title: title,
-    body: body,
+
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
   });
-  saveNotes(notes);
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log("New note added");
+  } else {
+    console.log("Note title taken!");
+  }
 };
 
 const saveNotes = function (notes) {
@@ -28,9 +38,18 @@ const loadNotes = function () {
   }
 };
 
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(function(note) {
+    return note.title !== title;
+  });
+  saveNotes(notesToKeep)
+}
+
 module.exports = {
   getNotes,
   addNote,
   saveNotes,
-  loadNotes
+  loadNotes,
+  removeNote
 };
